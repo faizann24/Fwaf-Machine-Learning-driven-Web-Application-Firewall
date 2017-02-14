1253c08ee992f00fcdc65071f0c5c5696ad6eb83
Liu Min <lmibrain@gmail.com>
2017-02-14 14:59:10 +0800

    refactor

--- script.py
+++ script.py
@@ -18,53 +18,41 @@ from sklearn.linear_model import LogisticRegression
 import urllib
 
 def getNGrams(query): #tokenizer function, this will make 3 grams of each query
-	tempQuery = str(query)
-	ngrams = []
-	for i in range(0,len(tempQuery)-3):
-		ngrams.append(tempQuery[i:i+3])
-	return ngrams
-  
-  
-filename = 'badqueries.txt'
-directory = str(os.getcwd())
-filepath = directory + "/" + filename
-data = open(filepath,'r').readlines()
-data = list(set(data))
-badQueries = []
-validQueries = []
-count = 0
-for d in data:
-        d = d.strip()
-        try:
-                d = str(urllib.unquote(d).decode('utf8'))   #converting url encoded data to simple string
-                badQueries.append(d)
-        except:
-                print 'decode ' + d + ' error'
+        tempQuery = str(query)
+        ngrams = []
+        for i in range(0,len(tempQuery)-3):
+                ngrams.append(tempQuery[i:i+3])
+        return ngrams
 
-filename = 'goodqueries.txt'
-directory = str(os.getcwd())
-filepath = directory + "/" + filename
-data = open(filepath,'r').readlines()
-data = list(set(data))
-for d in data:
-        d = d.strip()
-        try:
-                d = str(urllib.unquote(d).decode('utf8'))
-                validQueries.append(d)
-        except:
-                print 'decode ' + d + ' error'
+def getQueryFromFile(filename='badqueries.txt'):
+        directory = str(os.getcwd())
+        filepath = directory + "/" + filename
+        data = open(filepath,'r').readlines()
+        data = list(set(data))
+        queries = set()
+        for d in data:
+                d = d.strip()
+                try:
+                        d = str(urllib.unquote(d).decode('utf8'))   #converting url encoded data to simple string
+                        queries.add(d)
+                except:
+                        print 'decode ' + d + ' error'
+        return list(queries)
 
-badQueries = list(set(badQueries))
-tempvalidQueries = list(set(validQueries))
+
+
+badQueries = getQueryFromFile('badqueries.txt')
+tempvalidQueries = getQueryFromFile('goodqueries.txt')
 tempAllQueries = badQueries + tempvalidQueries
-bady = [1 for i in range(0,len(badQueries))]  #labels, 1 for malicious and 0 for clean
-goody = [0 for i in range(0,len(tempvalidQueries))]
-y = bady+goody
-queries = tempAllQueries
 
+ybad = np.ones(len(badQueries))
+ygood = np.zeros(len(tempvalidQueries))
+y = np.hstack((ybad, ygood))
+
+queries = tempAllQueries
 vectorizer = TfidfVectorizer(tokenizer=getNGrams) #converting data to vectors
 X = vectorizer.fit_transform(queries)
-print X[:10]
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) #splitting data
 
 lgs = LogisticRegression()
